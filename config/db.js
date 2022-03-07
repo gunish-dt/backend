9fdf6ee4135934ba47a1b42f262f7a15b034e8be
gunish-dt <gunish@devtron.ai>
2022-03-07 14:51:29 +0530

    added env support

--- config/db.js
+++ config/db.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
+//require("dotenv").config();
 
+//const db = "process.env.DB_BASE_URL";
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true });
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log(process.env.DB_BASE_URL);
 
     console.log("MongoDB is Connected...");
   } catch (err) {
