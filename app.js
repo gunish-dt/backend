10980c149f8e0a338f8bdbd724f2ab5bff0bebb1
gunish-dt <gunish@devtron.ai>
2022-03-07 23:20:33 +0530

    added headers

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ app.get("/", (req, res) => res.send("Hello world!"));
 // use Routes
 app.use("/api/books", books);
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
