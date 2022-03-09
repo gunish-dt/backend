2770cbfe5976f256bc07b4740ee2467d1626a446
gunish-dt <gunish@devtron.ai>
2022-03-09 22:38:46 +0530

    added response headers

--- app.js
+++ app.js
@@ -24,6 +24,17 @@ app.get("/", (req, res) => res.send("Hello world!"));
 // use Routes
 app.use("/api/books", books);
 
+app.options("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  return res.sendStatus(200);
+});
+
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
