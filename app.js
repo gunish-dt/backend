803123dc6dfd4bb9aaa8ee667d4d99061516c90b
gunish-dt <gunish@devtron.ai>
2022-03-09 22:20:45 +0530

    removed unnecesary header

--- app.js
+++ app.js
@@ -24,14 +24,6 @@ app.get("/", (req, res) => res.send("Hello world!"));
 // use Routes
 app.use("/api/books", books);
 
-app.options("/", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "POST");
-  res.header("Access-Control-Allow-Headers", "accept, content-type");
-  res.header("Access-Control-Max-Age", "1728000");
-  return res.sendStatus(200);
-});
-
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
